45f49b88a91ff730b2ad338c1c0da4743b71bb06
muhammad-2002 <masumbilla10104@gmail.com>
2024-03-24 16:55:16 +0600

    added all section

---
+++ src/components/noData.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NoData = ({ message, address, label }) => {
+  return (
+    <div className="flex justify-center items-center min-h-[calc(100vh-116px)] flex-col gap-5 ">
+      <h1 className="text-2xl font-bold">{message}</h1>
+      <Link
+        to={address}
+        className="relative inline-block px-4 py-2 font-medium group"
+      >
+        <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+        <span className="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-primary"></span>
+        <span className="relative text-black group-hover:text-white">
+          {label}
+        </span>
+      </Link>
+    </div>
+  );
+};
+
+export default NoData;
